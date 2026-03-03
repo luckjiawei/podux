e2497cf4e0edf3c4311b1d5b18e367a3d75bd127
刘嘉伟 <8473136@qq.com>
2026-03-03 10:18:31 +0800

    🎉 Starting version

--- pkg/buildinfo/buildinfo.go
+++ pkg/buildinfo/buildinfo.go
@@ -2,4 +2,4 @@ package buildinfo
 
 // AppVersion is the single source of truth for the application version.
 // Update this value to release a new version.
-const AppVersion = "1.0.0"
+const AppVersion = "0.1.0"
